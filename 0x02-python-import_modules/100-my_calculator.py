16d98e5f239e1ead43d637b682cf7eb05c23f4a6
Bright <66279388+bright-sablah@users.noreply.github.com>
2024-01-19 04:40:56 +0000

    match case method

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-from sys import argv
 if __name__ == "__main__":
-    if len(argv) != 4:
+    from calculator_1 import add, mul, div, sub
+    import sys
+
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
-    else:
-        a = int(argv[1])
-        b = int(argv[3])
-        if argv[2] == "+":
-            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-        elif argv[2] == "-":
-            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-        elif argv[2] == "*":
-            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-        elif argv[2] == "/":
-            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-        else:
+
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+
+    match op:
+        case '+':
+            print("{:d} {} {:d} = {:d}".format(a, op, b, add(a, b)))
+        case '-':
+            print("{} {} {} = {}".format(a, op, b, sub(a, b)))
+        case '/':
+            print("{} {} {} = {}".format(a, op, b, div(a, b)))
+        case '*':
+            print("{} {} {} = {}".format(a, op, b, mul(a, b)))
+        case _:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
